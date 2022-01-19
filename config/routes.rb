a7fb1c04b5897f4de9b81899772c81d5be1b98d0
Camille Eddy <45191356+nikkymill@users.noreply.github.com>
2022-01-19 11:44:37 -0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get 'pages/events'
   get 'pages/guides'
   get 'pages/tools'
+  get 'pages/careers'
+  get 'pages/ideas'
+  get 'pages/work'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
